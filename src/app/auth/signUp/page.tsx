95449fe7f03cd434a2e4bc00e59e203cc4c1cee2
Arihant <arihantjain1208@gmail.com>
2024-02-18 11:54:17 +0530

    singnup page

--- src/app/auth/signUp/page.tsx
+++ src/app/auth/signUp/page.tsx
@@ -2,9 +2,23 @@ import React from "react";
 
 const SignUp = () => {
   return (
-    <div>
-      SignUp
-      <h2>Arihant abhinav</h2>
+    <div className="text-black flex  bg-slate-500 w-full">
+      <div className="flex justify-center">
+        <form>
+          <div>
+            <label htmlFor="">Username</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">email</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label htmlFor="">Password</label>
+            <input type="password" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
