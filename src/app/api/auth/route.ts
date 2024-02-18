fe04b888c09ba4c30792599debad4045edec1548
Arihant <arihantjain1208@gmail.com>
2024-02-18 16:23:18 +0530

    signup success

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,4 +1,8 @@
-export default async function handler(req: any, res: any) {
-    res.send({ username: "abc", password: "hey" });
-  }
-  
+// export default async function handler(req: any, res: any) {
+//   res.send({ username: "abc", password: "hey" });
+// }
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  return new Response(JSON.stringify({ body, status: "success" }));
+}
--- src/app/auth/signUp/page.tsx
+++ src/app/auth/signUp/page.tsx
@@ -1,37 +1,42 @@
-"use client"
+"use client";
 import React from "react";
 import axios from "axios";
 
 const SignUp = async () => {
-  const handelsubmit = (event:any) => {
+  const handelsubmit = (event: any) => {
     event.preventDefault(); // Prevent the default form submission behavior
 
-    console.log("hey")
-    const URL = "http://localhost:3000/api/auth";
-    return axios
-      .get(URL)
+    console.log(event);
+    const URL = "/api/auth";
+    axios
+      .post(URL, {
+        username: event.target[0].value,
+        email: event.target[1].value,
+        password: event.target[2].value,
+      })
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
   return (
     <div className="text-black flex w-full  bg-slate-500">
       <div className="flex justify-center m-auto">
         <form onSubmit={handelsubmit}>
           <div>
-            <label htmlFor="">Username</label>
-            <input type="text" />
+            <label>Username</label>
+            <input type="text" name="name" />
           </div>
           <div>
-            <label htmlFor="">email</label>
-            <input type="email" />
+            <label>email</label>
+            <input type="email" name="email" />
           </div>
           <div>
-            <label htmlFor="">Password</label>
-            <input type="password" />
+            <label>Password</label>
+            <input type="password" name="password" />
           </div>
           <button type="submit">submit</button>
         </form>
